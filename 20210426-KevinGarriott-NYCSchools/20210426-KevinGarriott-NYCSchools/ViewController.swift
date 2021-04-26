5b605cb11278437da953b4e5ea5bf7f51d0fe493
garrkev <garriottkevin@gmail.com>
2021-04-26 17:24:00 -0500

    project set up

---
+++ 20210426-KevinGarriott-NYCSchools/20210426-KevinGarriott-NYCSchools/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  20210426-KevinGarriott-NYCSchools
+//
+//  Created by Kevin Garriott on 4/26/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
